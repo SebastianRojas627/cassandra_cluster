8d7713342114840944192beccbacefa45e70bd3a
Sebastian Rojas <sebosinaga@gmail.com>
2023-06-27 00:57:08 -0400

    added scripts

---
+++ node2.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+CONTAINER_NAME="node2"
+KEYSPACE_NAME="my_keyspace"
+TABLE_NAME="Person"
+CASSANDRA_PASSWORD="password"
+
+docker-compose exec "$CONTAINER_NAME" cqlsh --username=cassandra --password="$CASSANDRA_PASSWORD" -e "USE $KEYSPACE_NAME;"
+
+docker-compose exec "$CONTAINER_NAME" cqlsh --username=cassandra --password="$CASSANDRA_PASSWORD" -e "CONSISTENCY ONE;"

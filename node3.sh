ac2ba0b783c7124c36d01c600141c45e67579698
Sebastian Rojas <sebosinaga@gmail.com>
2023-06-27 08:47:18 -0400

    fix 5

--- node3.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export CQLSH_USERNAME=user
-export CQLSH_PASSWORD=password
-
-cqlsh --username=cassandra --password="$CASSANDRA_PASSWORD" -e "USE $KEYSPACE_NAME;"
-
-cqlsh --username=cassandra --password="$CASSANDRA_PASSWORD" -e "CONSISTENCY ONE;"
